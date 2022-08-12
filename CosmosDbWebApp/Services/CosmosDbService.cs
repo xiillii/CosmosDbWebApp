e338d1ea96ebd7a12255318823c54f8d6401160b
José Alonso <mail@josealonso.dev>
2022-08-12 15:00:48 -0500

    Adding CosmosDb Service

---
+++ CosmosDbWebApp/Services/CosmosDbService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CosmosDbWebApp.Services
+{
+    public class CosmosDbService
+    {
+        public CosmosDbService()
+        {
+        }
+    }
+}
+
---
+++ CosmosDbWebApp/Services/ICosmosDbService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CosmosDbWebApp.Models;
+
+namespace CosmosDbWebApp.Services;
+
+public interface ICosmosDbService
+{
+    Task<IEnumerable<Item>> GetItemsAsync(string query);
+    Task<Item> GetItemAsync(string id);
+    Task AddItemAsync(Item item);
+    Task UpdateItemAsync(string id, Item item);
+    Task DeleteItemAsync(string id);
+}
+
