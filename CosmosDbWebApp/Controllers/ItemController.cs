923b803d95fd66785b63b7d4f43ab69dac862b39
José Alonso <mail@josealonso.dev>
2022-08-12 19:57:03 -0500

    ItemController first attemp

---
+++ CosmosDbWebApp/Controllers/ItemController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CosmosDbWebApp.Models;
+using CosmosDbWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CosmosDbWebApp.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ICosmosDbService _cosmosDbService;
+
+        public ItemController(ICosmosDbService cosmosDbService)
+        {
+            _cosmosDbService = cosmosDbService;
+        }
+
+        [ActionName("Index")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _cosmosDbService.GetItemsAsync("SELECT * FROM C"));
+        }
+
+        [ActionName("Create")]
+        public IActionResult Create()
+        {
+            return View(new Item { Id = Guid.NewGuid().ToString() });
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync([Bind("Id,Name,Description,Completed")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                await _cosmosDbService.AddItemAsync(item);
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        [ActionName("Edit")]
+        public async Task<ActionResult> EditAsync(string id)
+        {
+            if (id == null)
+            {
+
+            }
+        }
+    }
+}
+
